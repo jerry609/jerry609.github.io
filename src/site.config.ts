6b9e656c5f4c16f711ba9fddbe871424fd129708
jerry609 <1772030600@qq.com>
2025-11-28 23:37:18 +0800

    更新头像和个人介绍

--- src/site.config.ts
+++ src/site.config.ts
@@ -26,7 +26,7 @@ export const theme: ThemeUserConfig = {
   },
   /** Set a logo image to show in the homepage. */
   logo: {
-    src: '/src/assets/avatar.png',
+    src: '/src/assets/jerry.jpg',
     alt: 'Avatar'
   },
 
