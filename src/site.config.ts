b03cfac37e2406a30629b8b263b31c42e486f82e
jerry <1772030600@qq.com>
2025-11-30 11:27:14 +0800

    refactor: make Waline page info conditional on configuration and disable Waline features by default.

--- src/site.config.ts
+++ src/site.config.ts
@@ -164,7 +164,7 @@ export const integ: IntegrationUserConfig = {
   },
   // Comment system
   waline: {
-    enable: true,
+    enable: false,
     // Server service link
     server: 'https://astro-theme-pure-waline.arthals.ink/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
@@ -172,8 +172,8 @@ export const integ: IntegrationUserConfig = {
     // Refer https://waline.js.org/en/reference/client/props.html
     additionalConfigs: {
       // search: false,
-      pageview: true,
-      comment: true,
+      pageview: false,
+      comment: false,
       locale: {
         reaction0: 'Like',
         placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
